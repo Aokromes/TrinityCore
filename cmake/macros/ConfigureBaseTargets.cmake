c447495d89819258bcb1f2cedfb9ab17965a6332
Aokromes <aokromes@gmail.com>
2018-03-10 15:24:28 +0100

    another try

--- cmake/macros/ConfigureBaseTargets.cmake
+++ cmake/macros/ConfigureBaseTargets.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
+# Copyright (C) 2008-2018 TrinityCore <https://www.trinitycore.org/>
 #
 # This file is free software; as a special exception the author gives
 # unlimited permission to copy and/or distribute it, with or without
@@ -10,6 +10,9 @@
 
 # An interface library to make the target com available to other targets
 add_library(trinity-compile-option-interface INTERFACE)
+
+# Use -std=c++11 instead of -std=gnu++11
+set(CXX_EXTENSIONS OFF)
 
 # An interface library to make the target features available to other targets
 add_library(trinity-feature-interface INTERFACE)
@@ -34,29 +37,43 @@ target_compile_features(trinity-feature-interface
 # This interface taget is set-up through the platform specific script
 add_library(trinity-warning-interface INTERFACE)
 
+# An interface used for all other interfaces
+add_library(trinity-default-interface INTERFACE)
+target_link_libraries(trinity-default-interface
+  INTERFACE
+    trinity-compile-option-interface
+    trinity-feature-interface)
+
+# An interface used for silencing all warnings
+add_library(trinity-no-warning-interface INTERFACE)
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  target_compile_options(trinity-no-warning-interface
+    INTERFACE
+      /W0)
+else()
+  target_compile_options(trinity-no-warning-interface
+    INTERFACE
+      -w)
+endif()
+
+# An interface library to change the default behaviour
+# to hide symbols automatically.
+add_library(trinity-hidden-symbols-interface INTERFACE)
+
 # An interface amalgamation which provides the flags and definitions
 # used by the dependency targets.
 add_library(trinity-dependency-interface INTERFACE)
 target_link_libraries(trinity-dependency-interface
   INTERFACE
-    trinity-compile-option-interface
-    trinity-feature-interface)
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-  target_compile_options(trinity-dependency-interface
-    INTERFACE
-      /W0)
-else()
-  target_compile_options(trinity-dependency-interface
-    INTERFACE
-      -w)
-endif()
+    trinity-default-interface
+    trinity-no-warning-interface
+    trinity-hidden-symbols-interface)
 
 # An interface amalgamation which provides the flags and definitions
 # used by the core targets.
 add_library(trinity-core-interface INTERFACE)
 target_link_libraries(trinity-core-interface
   INTERFACE
-    trinity-compile-option-interface
-    trinity-feature-interface
+    trinity-default-interface
     trinity-warning-interface)
